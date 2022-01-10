dfa3490229a1a150987305e6fd4b2a16498564e4
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-01-11 01:44:29 +0330

    second commit

--- store/models.py
+++ store/models.py
@@ -22,3 +22,15 @@ class Product(models.Model):
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
